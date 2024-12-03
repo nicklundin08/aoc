26afbab29d41ec7c2d7918ccebcb2fe3831c86c9
Nick <nick@Nicks-MBP.lan>
2024-12-03 00:06:42 -0600

    WIJP

--- 2024-golang/day2/operations.go
+++ 2024-golang/day2/operations.go
@@ -1,59 +1,30 @@
 package main
 
-type safetyCaclulator struct {
-	// Either increasing or decreasing
-	trend trend
-	// The row to inspect
-	row []int
-	// IndexA walks the array as the lagging index
-	indexA int
-	// IndexB walks the array as the leading index
-	indexB int
-	//Dampening is implemented as a lives remaning strategy. Normally during the walk we would increment both indexes, however if an pair of eleemnts doesnt pass, we "skip" the offending element by incrementing IndexB only
-	//[1,2,7,3,5]
-	// a b
-	//[1,2,7,3,5]
-	//   a b
-	//[1,2,7,3,5]
-	//   a   b        <---- b increemnts 1, a is not changed
-	//[1,2,7,3,5]
-	//       a b      <---- b increments 1, a is reset to b -1
-	livesRemaning int
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
 }
 
-func (sc *safetyCaclulator) check() {
-	if sc.indexA == len(sc.row) || sc.indexB == len(sc.row) {
-		return
-	}
-	if sc.livesRemaning == 0 {
-		return
-	}
-	a := sc.row[sc.indexA]
-	b := sc.row[sc.indexB]
-	doesPass := checkTwoElements(a, b, sc.trend)
-	sc.indexB++
+type chain struct {
+	checkedSafe []int
+	a           int
+	b           int
+	data        []int
+	index       int
+	skipsUsed   int
+	trend       trend
+}
+
+func processNext(c *chain) {
+	doesPass := checkTwoElements(c.a, c.b, c.trend)
 	if doesPass {
-		sc.indexA = sc.indexB - 1
-	} else {
-		sc.livesRemaning--
+		c.checkedSafe[c.index] = c.a
 	}
-	sc.check()
-}
-
-func createNewSafetyCalculator(row []int, withDampener bool, trend trend) *safetyCaclulator {
-	lives := 1
-
-	if withDampener {
-		lives++
-	}
-
-	return &safetyCaclulator{
-		row:           row,
-		trend:         trend,
-		indexA:        0,
-		indexB:        1,
-		livesRemaning: lives,
-	}
+	c.index++
+	c.a = c.data[c.index]
+	c.b = c.data[c.index+1]
 }
 
 type trend func(a int, b int) bool
@@ -79,25 +50,138 @@ func diff(a int, b int) int {
 }
 
 func checkTwoElements(a int, b int, trend trend) bool {
-	// fmt.Printf("Logging %d, %d.\n", a, b)
 	isGradual := gradual(a, b)
-	if !isGradual {
+	isTrending := trend(a, b)
+	retValue := isGradual && isTrending
+	// fmt.Printf("Checking two values %d and %d with trending %t, gradual %t, retval %t.\n", a, b, isTrending, isGradual, retValue)
+	return retValue
+}
+
+func computeTrend(a []int) trend {
+	head := a[0]
+	tail := a[1:]
+	diff := head - tail[0]
+	if diff > 0 {
+		return decreasing
+	}
+	return increasing
+}
+
+func checkSliceWithTrend(a []int, trend trend) bool {
+	if len(a) == 0 {
+		return true
+	}
+	if len(a) == 1 {
+		return true
+	}
+	head := a[0]
+	tail := a[1:]
+	doesPass := checkTwoElements(head, tail[0], trend)
+	if len(a) == 2 {
+		return doesPass
+	}
+	return doesPass && checkSliceWithTrend(tail, trend)
+}
+
+func checkSlice(a []int) bool {
+	trend := computeTrend(a)
+	return checkSliceWithTrend(a, trend)
+}
+
+func check2(data []int) int {
+	trend := computeTrend(data)
+	for index := 1; index < len(data); index++ {
+		a := data[index-1]
+		b := data[index]
+		doesPass := checkTwoElements(a, b, trend)
+		if !doesPass {
+			// fmt.Printf("Failure index %d for values %v.\n", index, data)
+			return index
+		}
+	}
+	// fmt.Printf("Failure index %d for values %v.\n", -1, data)
+	return -1
+}
+
+func makeSliceWithoutIndex(d []int, indexToOmit int) []int {
+	arr := make([]int, len(d)-1)
+	i := 0
+	for index, value := range d {
+		if indexToOmit != index {
+			arr[i] = value
+			i++
+		}
+	}
+	return arr
+}
+
+func check2WithSkips(data []int) bool {
+	if len(data) <= 1 {
+		return true
+	}
+	failureIndex := check2(data)
+	fmt.Println(failureIndex)
+	if failureIndex == -1 || failureIndex == len(data) {
+		return true
+	}
+
+	failureIndex2 := check2(makeSliceWithoutIndex(data, failureIndex-1))
+	if failureIndex2 == -1 {
+		return true
+	}
+
+	failureIndex3 := check2(makeSliceWithoutIndex(data, failureIndex))
+	if failureIndex3 == -1 {
+		return true
+	}
+	return false
+}
+
+func check(head []int, tail []int, skipsRemaining int) bool {
+	if skipsRemaining < 0 {
 		return false
 	}
-	isTrending := trend(a, b)
-	return isTrending
+
+	if len(tail) == 0 {
+		return true
+	}
+	fmt.Printf("Log head %d tail %v skips %d\n", head, tail, skipsRemaining)
+	lastHead := head[len(head)-1]
+	diff := lastHead - tail[0]
+	trend := increasing
+	if diff > 0 {
+		trend = decreasing
+	}
+	if len(tail) == 1 {
+		fmt.Println("x")
+		return skipsRemaining > 0 || checkTwoElements(lastHead, tail[0], trend)
+	}
+
+	doesPass := checkTwoElements(lastHead, tail[0], trend)
+
+	if doesPass {
+		fmt.Println("z")
+		// return check(tail[0], tail[1:], skipsRemaining)
+	}
+
+	skipsRemaining--
+	// doesPassIfSkipTail := check(head, tail[1:], skipsRemaining)
+	// skipTailAdditionalCheck := check(head, tail[1:1], skipsRemaining)
+	// doesPassIfSkipHead := check(tail[0], tail[1:], skipsRemaining)
+	// fmt.Printf("skip head: %t. Skip tail %t && %t.\n", doesPassIfSkipHead, doesPassIfSkipTail, skipTailAdditionalCheck)
+	// return doesPassIfSkipHead || (doesPassIfSkipTail && skipTailAdditionalCheck)
+	return false
 }
 
 func rowPasses(row []int, hasDampenerStrategy bool) bool {
 	if len(row) <= 2 {
 		return false
 	}
-	diff := row[0] - row[len(row)-1]
-	trend := increasing
-	if diff > 0 {
-		trend = decreasing
+	skips := 0
+	if hasDampenerStrategy {
+		skips = 1
 	}
-	sc := createNewSafetyCalculator(row, hasDampenerStrategy, trend)
-	sc.check()
-	return sc.livesRemaning > 0
+
+	return skips == 7
+	// return check(row[0], row[1:], skips)
 }
--- 2024-golang/day2/operations_test.go
+++ 2024-golang/day2/operations_test.go
@@ -5,72 +5,116 @@ import (
 	"testing"
 )
 
-func TestRecursiveCall(t *testing.T) {
+func TestCheck(t *testing.T) {
 	testCases := []struct {
+		reasoning   string
 		values      []int
 		expectation bool
 	}{
 		{
-			[]int{1, 2, 3},
-			true,
+			values:      []int{1, 2, 3},
+			expectation: true,
+			reasoning:   "Should pass without removing any level",
 		},
 		{
-			[]int{1, 1, 1},
-			false,
+			values:      []int{7, 1, 2, 3, 4},
+			expectation: true,
+			reasoning:   "Should pass if you remove the first level",
 		},
 		{
-			[]int{51, 54, 57, 60, 61, 64, 67, 64},
-			false,
+			values:      []int{0, 1, 2, 3, 10000},
+			expectation: true,
+			reasoning:   "Should pass if you remove the last level",
+		},
+		{
+			values:      []int{0, 5, 1, 300, 10000},
+			expectation: false,
+			reasoning:   "Should fail.",
+		},
+		{
+			values:      []int{7, 6, 4, 2, 1},
+			expectation: true,
+			reasoning:   "Safe without removing any values",
+		},
+		{
+			values:      []int{1, 2, 7, 8, 9},
+			expectation: false,
+			reasoning:   "Unsafe regardless",
+		},
+		{
+			values:      []int{9, 7, 6, 2, 1},
+			expectation: false,
+			reasoning:   "Unsafe regardless",
+		},
+		{
+			values:      []int{1, 3, 2, 4, 5},
+			expectation: true,
+			reasoning:   "Safe by removing the 2nd value",
+		},
+		{
+			values:      []int{8, 6, 4, 4, 1},
+			expectation: true,
+			reasoning:   "Save by removing the 3rd value",
+		},
+		{
+			values:      []int{1, 3, 6, 7, 9},
+			expectation: true,
+			reasoning:   "Safe",
 		},
 	}
+	// testCases = testCases[5:7]
 	for _, tc := range testCases {
-		result := rowPasses(tc.values, false)
+		fmt.Println("-------------")
+		result := check2WithSkips(tc.values)
 		if result != tc.expectation {
-			t.Errorf("Expected %t but found %t with values %v\n", tc.expectation, result, tc.values)
+			t.Errorf("Expected %t but found %t with values %v, reasoning %s.\n", tc.expectation, result, tc.values, tc.reasoning)
 		}
 	}
 }
 
 func TestFileWithNoDampener(t *testing.T) {
-	fmt.Println("----------------------- Hello world")
+	// fmt.Println("----------------------- Hello world")
 	path := "./input.txt"
 	result, err := readListsFromFile(path)
 	if err != nil {
 		fmt.Print(err)
 		return
 	}
-	fmt.Printf("Total number of lines in file %d.\n", len(result))
+	// fmt.Printf("Total number of lines in file %d.\n", len(result))
 	passCount := 0
 	for _, row := range result {
-		if rowPasses(row, false) {
+		if check2WithSkips(row) {
 			passCount++
 		}
 	}
-	fmt.Printf("pass count: %d.\n", passCount)
-	fmt.Println("----------------------- Goodbye world")
+	// fmt.Printf("pass count: %d.\n", passCount)
+	// fmt.Println("----------------------- Goodbye world")
 	if passCount != 224 {
 		t.Errorf("Found %d pass count instead of 224", passCount)
 	}
 }
 
 func TestFileWithDampener(t *testing.T) {
-	fmt.Println("----------------------- Hello world")
+	// fmt.Println("----------------------- Hello world")
 	path := "./input.txt"
 	result, err := readListsFromFile(path)
 	if err != nil {
 		fmt.Print(err)
 		return
 	}
-	fmt.Printf("Total number of lines in file %d.\n", len(result))
+	// fmt.Printf("Total number of lines in file %d.\n", len(result))
 	passCount := 0
 	for _, row := range result {
-		if rowPasses(row, true) {
+		if check2WithSkips(row) {
 			passCount++
 		}
 	}
-	fmt.Printf("pass count: %d.\n", passCount)
-	fmt.Println("----------------------- Goodbye world")
-	if passCount != 224 {
-		t.Errorf("Found %d pass count instead of 224", passCount)
+	// fmt.Printf("pass count: %d.\n", passCount)
+	// fmt.Println("----------------------- Goodbye world")
+	if passCount <= 224 {
+		t.Errorf("Should be greater than 224. %d", passCount)
+	}
+	if passCount > 350 {
+		t.Errorf("Should be less than 350. %d", passCount)
 	}
 }
